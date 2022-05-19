0cffa68d1b4543d3e99ee91524aecebcbd4a6fb7
Diogo Pereira <a036751@ismai.pt>
2022-05-20 00:54:49 +0100

    load foto da db

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -1,13 +1,41 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import Loader from "../components/Loader";
+import { getImageRoute } from "../utils/APIRoutes";
+import { Buffer } from "buffer";
 
-function Chat() {
+export default function Chat() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [items, setItems] = useState([]);
+  const [currentUser, setCurrentUser] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      const user = JSON.parse(localStorage.getItem("user"));
+
+      setCurrentUser(user);
+      const data = await axios.get(`${getImageRoute}/${user._id}`);
+      console.log("dados user : " + data.image);
+      setItems(data);
+      console.log("user:" + user._id);
+      setIsLoading(false);
+    }
+    fetchData();
+  }, []);
+
   return (
     <>
-      <div>Chat</div>
-      <Loader />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          {" "}
+          <div>Chat</div>
+          <Loader />
+          {console.log(items)}
+          <img src={items.data.image} alt="teste" />
+        </>
+      )}
     </>
   );
 }
-
-export default Chat;
