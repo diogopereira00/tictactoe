f03893f12b8f773524ca10be0baa0af9f4fe7678
Diogo Pereira <a036751@ismai.pt>
2022-05-23 23:28:17 +0100

    fix bug no showgames

--- public/src/components/ShowGames.jsx
+++ public/src/components/ShowGames.jsx
@@ -4,7 +4,7 @@ import GameCard from "./GameCard";
 
 export default function ShowGames() {
   return (
-    <SimpleGrid minChildWidth="335px">
+    <SimpleGrid minChildWidth="350px" spacing={2} maxH="95vh" overflowY="auto">
       <GameCard status="open" /> <GameCard status="open" /> <GameCard status="open" />{" "}
       <GameCard status="open" />
       <GameCard status="open" />
