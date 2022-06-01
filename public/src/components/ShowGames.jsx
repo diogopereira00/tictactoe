d5f99236d5704fc382b6b625cada50613ba4a447
Diogo Pereira <a036751@ismai.pt>
2022-06-01 01:44:59 +0100

    showgames limpo

--- public/src/components/ShowGames.jsx
+++ public/src/components/ShowGames.jsx
@@ -2,19 +2,14 @@ import { SimpleGrid } from "@chakra-ui/react";
 import React from "react";
 import GameCard from "./GameCard";
 
-export default function ShowGames() {
+export default function ShowGames(props) {
   return (
     <>
       <SimpleGrid minChildWidth="350px" spacing={2} maxH="95vh" overflowY="auto">
-        <GameCard status="open" />
-        <GameCard status="open" />
-        <GameCard status="open" />
-        <GameCard status="open" />
-        <GameCard status="open" />
-        <GameCard status="open" />
-        <GameCard status="open" />
+        <GameCard image={props.image} username={props.username} id={props.id} status="create" />
         <GameCard status="open" />
         <GameCard status="running" />
+        <GameCard status="closed" />
         <GameCard status="closed" />
       </SimpleGrid>
     </>
