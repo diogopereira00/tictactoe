6d28c690e0f4bc2bd6d52ac4a71c093a71b39a11
Diogo Pereira <a036751@ismai.pt>
2022-06-02 19:00:09 +0100

    getAllUsers rota

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,16 @@
-const { register, login, setAvatar, getCurrentUser } = require("../controllers/userController");
+const {
+  register,
+  login,
+  setAvatar,
+  getCurrentUser,
+  getAllUsers,
+} = require("../controllers/userController");
 
 const router = require("express").Router();
 router.post("/register", register);
 router.post("/login", login);
 router.post("/setAvatar/:id", setAvatar);
 router.get("/getCurrentUser/:id", getCurrentUser);
+router.get("/getAllUsers", getAllUsers);
 
 module.exports = router;
